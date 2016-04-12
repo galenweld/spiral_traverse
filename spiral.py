9f671c3f06171b0ea81091f79372d4af63a5963c
Galen Weld <galen.weld@gmail.com>
2016-04-11 21:47:52 -0400

    fixed bug by removing ' ' from ' right'

--- spiral.py
+++ spiral.py
@@ -1,8 +1,11 @@
-from copy import copy as copy
+from copy import deepcopy as copy
+import pprint
 
 four_x_four = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
 filled_four_x_four = [[1, 2, 3, 4], ["C", "D", "E", 5], ["B", "G", "F", 6], ["A", 9, 8, 7]]
 filled_two_x_two = [[0, 1], [3, 2]]
+
+printer = pprint.PrettyPrinter()
 
 
 next_side = {"top":"right", "right":"bottom", "bottom":"left", "left":"top"}
@@ -23,7 +26,7 @@ def trim(m, side):
 	if side == "bottom": return m[:len(m)-1]
 	out = []
 	for row in m:
-		out.append(row[:len(row)-1] if side == "right" else row[:])
+		out.append(row[:len(row)-1] if side == "right" else row[1:])
 	return out
 
 
@@ -41,7 +44,7 @@ def spiral(m, side="top"):
 	if m == [[]] or m == []: return []
 
 	side_contents = []
-	if side == "top" or side == " right": side_contents = copy(trimmed(m, side))
+	if side == "top" or side == "right": side_contents = copy(trimmed(m, side))
 	if side == "bottom" or side == "left": side_contents = list(reversed(trimmed(m, side)))
 	print side + ", " + str(side_contents) + " of"
 	print_matrix(m)
